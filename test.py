a12f14172af9d860b18f16a50d5c5d3134f08ee2
paul_dragon <67547959+stu-zhangxl@users.noreply.github.com>
2022-11-15 15:27:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-from nets.retinaface import RetinaFace
+from retinaface import Retinaface
 from utils.config import cfg_mnet, cfg_re50
+import os
+import cv2
 
 if __name__ == '__main__':
-    model = RetinaFace(cfg_mnet,backbone="mobilenet")
-    model.summary()
+    model = Retinaface()
 
-    for i,layer in enumerate(model.layers):
-        print(i,layer.name)
+    # model_path = "model_data/retinaface_mobilenet025.h5"
+    #------------------------------------------------------#
+    #   载入预训练权重
+    #------------------------------------------------------#
+    # print('Load weights {}.'.format(model_path))
+    # model.load_weights(model_path, by_name=True, skip_mismatch=True)
+    img = 'img/timg.jpg'
+    image = cv2.imread(img)
+    # image = cv2.resize(image,(224, 224))
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    r_image = model.detect_image(image)
+    r_image = cv2.cvtColor(r_image,cv2.COLOR_RGB2BGR)
+    cv2.imshow("after",r_image)
+    cv2.waitKey(0)
